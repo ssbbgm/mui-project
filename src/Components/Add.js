a4d54d8b55c1f04007957f51d2d32956c26074f9
Sherreka <rekadarnb@gmail.com>
2022-07-30 16:08:47 -0400

    adding modal

--- src/Components/Add.js
+++ src/Components/Add.js
@@ -1,15 +1,27 @@
-import { Tooltip, Fab } from '@mui/material';
+import { Tooltip, Fab, Modal, Typography, Box, style } from '@mui/material';
 import { Add as AddIcon} from '@mui/icons-material'
-import React from 'react';
+import React, { useState } from 'react';
 
 
 function Add() {
+    const [open, setOpen] = useState(false);
     return (
-        <Tooltip title="Delete">
+        <>
+        <Tooltip onClick={(e)=>setOpen(true)} title="New Post" sx={{position: 'fixed', bottom: 20, left:{xs: 'calc(50% - 25px)', md: 30}}}>
             <Fab color="primary" aria-label="add">
                 <AddIcon />
             </Fab>
         </Tooltip>
+        <Modal
+        open={open}
+        onClose={(e)=>setOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description">
+        <Box >
+          Hello
+        </Box>
+      </Modal>
+      </>
     );
 }
 
