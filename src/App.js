5c63e29e6a12a774cab011d12e7f559a7093d145
Sherreka <rekadarnb@gmail.com>
2022-07-30 10:34:21 -0400

    adding navbar

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import Sidebar from './Components/Sidebar';
 import Navbar from './Components/Navbar';
 import Rightbar from './Components/Rightbar';
 import Feed from './Components/Feed';
-import {Box, Container, Stack} from '@mui/material';
+import {Box, Stack} from '@mui/material';
 
 
 function App() {
 
   return (
       <Box>
-        {/* <Navbar /> */}
+        <Navbar />
         <Stack direction='row' spacing={2} justifyContent='space-between'>
           <Sidebar />
           <Feed />
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,30 @@
+import { AppBar, Toolbar, styled, Topography, Typography, Icon } from "@mui/material";
+import { Pets } from '@mui/icons-material'
 import React from "react";
+
+const StyledToolbar = styled(Toolbar)({
+    display: 'flex',
+    justifyContent: 'space-between'
+})
+
+const Search = styled('div')(({ theme }) => ({
+    backgroundColor: 'white'
+}))
+
+const Icons = styled('div')(({ theme }) => ({
+    backgroundColor: 'white'
+}))
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <AppBar position='sticky'>
+            <StyledToolbar>
+                <Typography variant="h6" sx={{ display: {xs: 'none', sm: 'block'}}}>LAMA DEV</Typography>
+                <Pets sx={{ display: {xs: 'block', sm: 'none'}}}/>
+                <Search>search</Search>
+                <Icons>icons</Icons>
+            </StyledToolbar>
+        </AppBar>
     )
 };
 
