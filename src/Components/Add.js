1e42e5f878b3f257f7b4c98b4d083133b5301432
Sherreka <rekadarnb@gmail.com>
2022-07-30 16:27:32 -0400

    adding to add post

--- src/Components/Add.js
+++ src/Components/Add.js
@@ -1,7 +1,19 @@
-import { Tooltip, Fab, Modal, Typography, Box, style } from '@mui/material';
+import { Tooltip, Fab, Modal, Typography, Box, styled, Avatar, TextField, Stack} from '@mui/material';
 import { Add as AddIcon} from '@mui/icons-material'
 import React, { useState } from 'react';
 
+const StyledModal = styled(Modal)({
+    display:'flex',
+    alignItems:'center',
+    justifyContent: 'center'
+});
+
+const UserBox = styled(Box)({
+    display: "flex",
+    alignItems: "center",
+    gap: "10px",
+    marginBottom: "20px",
+});
 
 function Add() {
     const [open, setOpen] = useState(false);
@@ -12,15 +24,39 @@ function Add() {
                 <AddIcon />
             </Fab>
         </Tooltip>
-        <Modal
-        open={open}
-        onClose={(e)=>setOpen(false)}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description">
-        <Box >
-          Hello
+        <StyledModal
+            open={open}
+            onClose={(e)=>setOpen(false)}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+        >
+        <Box width={400} height={280} bgcolor='white' borderRadius={5}>
+          <Typography variant='h6' color='gray' textAlign='center' mt={2}>Create post</Typography>
+          <UserBox>
+            <Avatar
+                src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                sx={{ width: 30, height: 30, ml: 2 }}
+            />
+            <Typography fontWeight={500} variant="span">
+                John Doe
+            </Typography>
+          </UserBox>
+                <TextField
+                id="standard-multiline-static"
+                multiline
+                rows={4}
+                placeholder="'What's on your mind?"
+                variant="standard"
+                sx={{ width:'90%', m: 2}}
+            />
+            <Stack direction="row" gap={1} mt={2} mb={3}>
+                <EmojiEmotions color="primary" />
+                <Image color="secondary" />
+                <VideoCameraBack color="success" />
+                <PersonAdd color="error" />
+          </Stack>
         </Box>
-      </Modal>
+      </StyledModal>
       </>
     );
 }
